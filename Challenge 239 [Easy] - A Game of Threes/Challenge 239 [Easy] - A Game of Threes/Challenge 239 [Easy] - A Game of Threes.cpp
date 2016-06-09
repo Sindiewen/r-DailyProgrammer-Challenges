14c52a5d88b0b737a0584e75b1b3b7d860559874
Rachel <sindiewen@gmail.com>
2016-06-09 16:23:16 -0700

    Framework for Threes
    Added the base framework for A Game of Threes

--- Challenge 239 [Easy] - A Game of Threes/Challenge 239 [Easy] - A Game of Threes/Challenge 239 [Easy] - A Game of Threes.cpp
+++ Challenge 239 [Easy] - A Game of Threes/Challenge 239 [Easy] - A Game of Threes/Challenge 239 [Easy] - A Game of Threes.cpp
@@ -1,11 +1,39 @@
 // Challenge 239 [Easy] - A Game of Threes.cpp : Defines the entry point for the console application.
 //
 
+///<Summary>
+/*\
+	The input is a single number: the number at which the game starts. Write a program that plays the Threes game, and 
+	outputs a valid sequence of steps you need to take to get to 1. Each step should be output as the number you start 
+	at, followed by either -1 or 1 (if you are adding/subtracting 1 before dividing), or 0 (if you are just dividing). 
+	The last line should simply be 1.
+*/
+///</Summary>
+
+// Preprocessors
 #include "stdafx.h"
+#include <iostream>
+
+//Namespace
+using namespace std;
+// Function Prototypes
+void add1(int &num);	// Adds 1 to num
+void sub1(int &num);	// Subtracts 1 from num
 
 
 int main()
 {
-    return 0;
+	// Ends program (For Windows)
+	system("pause");
+	return 0;
 }
 
+void add1(int &num)
+{
+
+}
+
+void sub1(int &num)
+{
+
+}
